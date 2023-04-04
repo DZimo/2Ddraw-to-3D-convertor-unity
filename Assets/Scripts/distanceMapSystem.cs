81952a306d4ed914caaa6c8915181251446dc124
DZimo <belabedmohamednazim@gmail.com>
2023-04-04 15:20:24 +0200

    First commit

---
+++ Assets/Scripts/distanceMapSystem.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class distanceMapSystem : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject finalPosition, gameCarPosition;
+    [SerializeField]
+    private Slider sliderRaceMap;
+    private float positionDifferenceFirst, positionPercentage;
+
+    private void Start()
+    {
+        positionDifferenceFirst = finalPosition.transform.position.x - gameCarPosition.transform.position.x;
+    }
+    void Update()
+    {
+        calculatePositionClamp();
+    }
+
+    private void calculatePositionClamp()
+    {
+        positionPercentage = gameCarPosition.transform.position.x / positionDifferenceFirst;
+        updateRaceMapPosition(positionPercentage);
+    }
+
+    private void updateRaceMapPosition(float positionDifferenceClamped)
+    {
+        sliderRaceMap.value = positionDifferenceClamped;
+    }
+}
